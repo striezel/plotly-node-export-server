3b3d8fac16e963914c0c43b1c8dff2d0ead4fcbf
Dirk Stolle <striezel-dev@web.de>
2024-12-18 15:23:07 +0100

    tests: minor cleanup

--- export-server/tests/charts/scientific/heatmap.test.js
+++ export-server/tests/charts/scientific/heatmap.test.js
@@ -375,7 +375,6 @@ describe('scientific charts, part 2', () => {
       });
     });
 
-    // TODO: Heatmap with Unequal Block Sizes - https://plotly.com/javascript/heatmaps/#heatmap-with-unequal-block-sizes
     it('Heatmap with Unequal Block Sizes', () => {
       const options = {
         port: 3000,
@@ -530,44 +529,5 @@ describe('scientific charts, part 2', () => {
         });
       });
     });
-
-/*
-    it('request with different data', () => {
-      const options = {
-        port: 3000,
-        host: 'localhost',
-        method: 'POST'
-      };
-
-      const req = http.request(options);
-      // Data taken from https://plotly.com/javascript/... example.
-      const payload = '{ "x": ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"], "y": [1, 3, 6], "type": "scatter" }';
-      req.write(payload);
-      req.end();
-
-      req.on('response', (response) => {
-        assert.strictEqual(200, response.statusCode);
-        let body = '';
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
-        response.on('end', () => {
-          assert.ok(body.startsWith('<svg'));
-          assert.ok(body.endsWith('</svg>'));
-
-          // Browser-based answer is something like
-          // <svg ..
-
-          // Current server response is visually almost the same.
-          // <svg ...
-
-          // Check partial matches, avoiding the random id bits.
-          assert.ok(body.indexOf('') >= 0);
-          assert.ok(body.indexOf('') > 0);
-          assert.ok(body.indexOf('') > 0);
-        });
-      });
-    });
-    */
   });
 });
