f5d7ee6df3b9337151ebff6bac1994cff4ef3aa0
Dirk Stolle <striezel-dev@web.de>
2025-02-16 14:58:19 +0100

    tests: shorten name of file for bubble chart tests

--- export-server/tests/charts/basic/bubble.test.js
+++ export-server/tests/charts/basic/bubble.test.js
@@ -436,34 +436,5 @@ describe('basic charts, part 5', () => {
         });
       });
     });
-
-/*
-    it('request with different data', () => {
-      const options = {
-        port: 3000,
-        host: 'localhost',
-        method: 'POST'
-      };
-
-      const req = http.request(options);
-      const payload = '{ "x": ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"], "y": [1, 3, 6], "type": "scatter" }';
-      req.write(payload);
-      req.end();
-
-      req.on('response', (response) => {
-        assert.strictEqual(200, response.statusCode);
-        let body = '';
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
-        response.on('end', () => {
-          assert.ok(body.startsWith('<svg'));
-          assert.ok(body.endsWith('</svg>'));
-          assert.ok(body.indexOf('width="700"') > 0);
-          assert.ok(body.indexOf('height="400"') > 0);
-        });
-      });
-    });
-    */
   });
 });
